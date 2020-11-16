bc6bee6df5d6d12e1781d3ebf6f1e50abc817918
EvertonHimself <evertondeoliveira@gmail.com>
2020-11-16 11:46:13 -0300

    Finished programming the Numpad.
    Programmed the numpad.

---
+++ Interface/Numpad.gd
@@ -0,0 +1,54 @@
+extends Popup
+
+var combination = [0,4,5,1]
+var guess = []
+
+onready var display = $VBoxContainer/DisplayContainer/Display
+onready var light = $VBoxContainer/ButtonContainer/GridContainer/StatusLight
+
+signal combination_correct
+	
+func _ready():
+	connect_buttons()
+
+func connect_buttons():
+	for child in $VBoxContainer/ButtonContainer/GridContainer.get_children():
+		if child is Button:
+			child.connect("pressed", self, "Button_pressed", [child.text])
+			
+func Button_pressed(button):
+	if button == "OK":
+		check_guess()
+	else:
+		enter(int(button))
+
+func check_guess():
+	if guess == combination:
+		$AudioStreamPlayer2D.stream = load("res://SFX/threeTone1.ogg")
+		$AudioStreamPlayer2D.play()
+		light.texture = load("res://GFX/Interface/PNG/dotGreen.png")
+		#emit_signal("combination_correct")
+		$Timer.start()
+	else:
+		reset_lock()
+
+func enter(button):
+	$AudioStreamPlayer2D.stream = load("res://SFX/twoTone1.ogg")
+	$AudioStreamPlayer2D.play()
+	guess.append(button)
+	update_display()
+
+func update_display():
+	display.text = PoolStringArray(guess).join("")
+	if guess.size() == combination.size():
+		check_guess()
+
+func reset_lock():
+	light.texture = load("res://GFX/Interface/PNG/dotRed.png")
+	display.text = ""
+	guess.clear()
+
+
+func _on_Timer_timeout():
+	emit_signal("combination_correct")
+	reset_lock()
