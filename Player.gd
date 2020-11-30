54a15b98a7f16060317c1122ad25234e83a3380b
EvertonHimself <evertondeoliveira@gmail.com>
2020-11-30 11:04:39 -0300

    Created "the box".
    Created "the box" for the player to hide in.

--- Player.gd
+++ Player.gd
@@ -2,6 +2,15 @@ extends "res://Characters/TemplateCharacter.gd"
 
 var motion = Vector2()
 
+const PLAYER_SPRITE = "res://GFX/PNG/Hitman 1/hitman1_stand.png"
+const BOX_SPRITE = "res://GFX/PNG/Tiles/tile_130.png"
+const PLAYER_OCCLUDER = "res://Characters/HumanOccluder.tres"
+const BOX_OCCLUDER = "res://Characters/BoxOccluder.tres"
+const PLAYER_LIGHT = "res://GFX/PNG/Hitman 1/hitman1_stand.png"
+const BOX_LIGHT = "res://GFX/PNG/Tiles/tile_130.png"
+
+var disguised = false
+
 func _physics_process(delta):
 	update_movement()
 	move_and_slide(motion)
@@ -29,3 +38,26 @@ func update_movement():
 func _input(event):
 	if Input.is_action_just_pressed("flashlight"):
 		get_tree().call_group("Interface", "cycle_vision_mode")
+	# Toggle the box.
+	if Input.is_action_justpressed("toggle_disguise"):
+		toggle_disguise()
+
+func toggle_disguise():
+	if disguised:
+		reveal()
+	else:
+		disguise()
+
+func reveal():
+	$Sprite.texture = load(PLAYER_SPRITE)
+	$LightOccluder2D.occluder = load(PLAYER_OCCLUDER)
+	$Light2D.texture = load(PLAYER_LIGHT)
+	disguised = false
+	collision_layer = 16
+
+func disguise():
+	$Sprite.texture = load(BOX_SPRITE)
+	$LightOccluder2D.occluder = load(BOX_OCCLUDER)
+	$Light2D.texture = load(BOX_LIGHT)
+	disguised = false
+	collision_layer = 1
