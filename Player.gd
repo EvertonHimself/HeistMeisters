af989c360ef95d164cdaf799179e55b5076c1549
EvertonHimself <evertondeoliveira@gmail.com>
2020-12-07 10:43:17 -0300

    Make the disguise dangerous.
    Make being in disguise dangerous to the player, making he move slowly when disguised.

--- Player.gd
+++ Player.gd
@@ -11,9 +11,20 @@ const BOX_LIGHT = "res://GFX/PNG/Tiles/tile_130.png"
 
 var disguised = false
 
+# The disguise slow down.
+export var disguise_slowdown = 0.25
+# Velocity multiplier.
+var velocity_multiplier = 1
+var disguise_duration = 5
+export var number_of_disguises = 3
+
 func _physics_process(delta):
 	update_movement()
-	move_and_slide(motion)
+	move_and_slide(motion * velocity_multiplier)
+	# Updates the label.
+	if disguised:
+		$DisguiseLabel.text = str($Timer.time_left).pad_decimals(2)
+		$DisguiseLabel.rect_rotation = - rotation_degrees
 	
 func update_movement():
 	look_at(get_global_mouse_position())
@@ -39,19 +50,23 @@ func _input(event):
 	if Input.is_action_just_pressed("flashlight"):
 		get_tree().call_group("Interface", "cycle_vision_mode")
 	# Toggle the box.
-	if Input.is_action_justpressed("toggle_disguise"):
+	if Input.is_action_just_pressed("toggle_disguise"):
 		toggle_disguise()
 
 func toggle_disguise():
 	if disguised:
 		reveal()
-	else:
+	elif number_of_disguises > 0:
 		disguise()
 
 func reveal():
 	$Sprite.texture = load(PLAYER_SPRITE)
 	$LightOccluder2D.occluder = load(PLAYER_OCCLUDER)
 	$Light2D.texture = load(PLAYER_LIGHT)
+	$DisguiseLabel.hide()
+	
+	velocity_multiplier = 1
+	
 	disguised = false
 	collision_layer = 16
 
@@ -59,5 +74,15 @@ func disguise():
 	$Sprite.texture = load(BOX_SPRITE)
 	$LightOccluder2D.occluder = load(BOX_OCCLUDER)
 	$Light2D.texture = load(BOX_LIGHT)
-	disguised = false
-	collision_layer = 1
+	$DisguiseLabel.show()
+	
+	velocity_multiplier = disguise_slowdown
+	number_of_disguises -= 1
+	
+	disguised = true
+	collision_layer = 16
+	$Timer.start()
+
+func _ready():
+	$Timer.wait_time = disguise_duration
+	reveal()
